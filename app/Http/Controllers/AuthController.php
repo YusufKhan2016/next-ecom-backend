59434653518db4ddc7dff9bd408dc8732bf16d1f
Hasan Yusuf Khan <hasanrafsun5@gmail.com>
2026-07-15 00:20:54 +0600

    authentication enviroment setup done & login controller making done

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) 
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Credentials'
+            ]);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'token' => $token,
+            'user' => $user,
+            'message' => 'Login Successful'
+        ]);
+    }
+    public function user(Request $request) 
+    {
+
+    }
+    public function logout(Request $request) 
+    {
+
+    }
+    public function changePassword(Request $request) 
+    {
+
+    }
+}
