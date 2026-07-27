ffb9fa43db731666e743e9bb3020ec6f00d0cd25
Hasan Yusuf Khan <hasanrafsun5@gmail.com>
2026-07-28 00:54:27 +0600

    api comments updated

--- routes/api.php
+++ routes/api.php
@@ -11,32 +11,32 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->prefix('/auth')->group(function () {
 
-    // ==========================
-    // authentication management
-    // ==========================
+    // ===============================================
+    //      authentication management
+    // ===============================================
 
     Route::get('/get-user', [AuthController::class, 'getUser']);
     Route::get('/logout', [AuthController::class, 'logout']);
     Route::post('/change-password', [AuthController::class, 'changePassword']);
 
-    // ==========================
-    // role management
-    // ==========================
+    // ==============================================
+    //      role management
+    // ==============================================
 
     Route::get('/get-roles-list', [RoleController::class, 'index']);
     Route::post('/save-update-role', [RoleController::class, 'store']);
     Route::get('/get-role-by-id/{role}', [RoleController::class, 'show']);
     Route::delete('/delete-role/{id}', [RoleController::class, 'destroy']);
 
-    // ==========================
-    // Permission management
-    // ==========================
+    // =============================================
+    //      Permission management
+    // =============================================
 
     Route::get('/get-permissions-list', [PermissionController::class, 'index']);
 
-    // ==========================
-    // user management
-    // ==========================
+    // ============================================
+    //      user management
+    // ============================================
 
     Route::get('/get-users-list', [UserController::class, 'index']);
     Route::post('/save-update-user', [UserController::class, 'store']);
